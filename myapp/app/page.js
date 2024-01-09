95b9c7973bfd86faebb06b6155864302c137087d
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-01-09 16:41:51 -0500

    update

--- myapp/app/page.js
+++ myapp/app/page.js
@@ -5,6 +5,12 @@ export default function Home() {
   return (
    <main className='mx-auto max-w-screen-lg '>
     <Hero />
+    <div className="divider"></div> 
+    <div class="flex flex-col text-center w-full mb-20 mt-20">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+    </div>
+    jasfvnajsfnk jand  
    </main>
   )
 }
--- myapp/components/Hero/Hero.js
+++ myapp/components/Hero/Hero.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <section className=''>
-        <div className='text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20'>
-            <h2 className='text-3xl font-extrabold text-black sm:text-4xl'>
-                <span className='block'>The Fitness</span>
-                <span
-                className='block text-orange-500'
-                >World</span>
-            </h2>
-            <p className='text-xl mt-4 max-w-md mx-auto text-gray-400'>
-                A Space To Share All Things Fitness Related
-            </p>
-        </div>
-    </section>
-  )
-}
+<section class="text-gray-600 body-font">
+  <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="../hero.png" />
+    <div class="text-center lg:w-2/3 w-full">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
+      <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+      </div>
+    </div>
+  </div>
+</section>
+  );
+};
 
-export default Hero
+export default Hero;
